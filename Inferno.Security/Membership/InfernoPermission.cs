0914ee4c75f32152d8fa249e07c006e5c6772c0b
Matt <gordon-matt@hotmail.com>
2023-02-11 12:11:58 +1030

    Updated NuGet packages

--- Inferno.Security/Membership/InfernoPermission.cs
+++ Inferno.Security/Membership/InfernoPermission.cs
@@ -1,15 +1,15 @@
-﻿using Extenso.Data.Entity;
+﻿//using Extenso.Data.Entity;
 
-namespace Inferno.Security.Membership
-{
-    public class InfernoPermission : BaseEntity<string>
-    {
-        public int? TenantId { get; set; }
+//namespace Inferno.Security.Membership
+//{
+//    public class InfernoPermission : BaseEntity<string>
+//    {
+//        public int? TenantId { get; set; }
 
-        public string Name { get; set; }
+//        public string Name { get; set; }
 
-        public string Category { get; set; }
+//        public string Category { get; set; }
 
-        public string Description { get; set; }
-    }
-}
+//        public string Description { get; set; }
+//    }
+//}
